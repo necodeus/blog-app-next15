fcebbc1e1d7c5f996db871ee14a9b0b4515b3d24
Dawid Smulewicz <dawid.smulewicz@grupawp.pl>
2024-07-21 18:50:09 +0200

    fix: navigation

--- components/components/Navigation/Navigation.tsx
+++ components/components/Navigation/Navigation.tsx
@@ -1,21 +1,51 @@
+'use client';
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import Logo from "../Logo/Logo";
 
+const links = [
+  {
+    href: "/",
+    label: "Artykuły",
+  },
+  {
+    href: "/autorzy",
+    label: "Autorzy",
+  },
+  {
+    href: "/projekty",
+    label: "Projekty",
+  },
+  {
+    href: "/kontakt",
+    label: "Kontakt",
+  },
+];
+
 export const Navigation = () => {
-	return (
-        <div className="component-border-bottom flex font-jost text-[16px] h-[50px]" >
-            <div style={{
-                display: 'flex',
-                gap: '30px',
-                alignItems: 'center',
-                margin: '0 30px',
-            }}>
-                <Logo className="mr-[20px]" height={25} />
-                <Link className="font-medium" href="/">Artykuły</Link>
-                <Link className="" href="/autorzy">Autorzy</Link>
-                <Link className="" href="/projekty">Projekty</Link>
-                <Link className="" href="/kontakt">Kontakt</Link>
-            </div>
-        </div>
-	);
+    const pathname = usePathname();
+
+  return (
+    <div className="component-border-bottom flex font-jost text-[16px] h-[50px]">
+      <div
+        style={{
+          display: "flex",
+          gap: "30px",
+          alignItems: "center",
+          margin: "0 30px",
+        }}
+      >
+        <Logo className="mr-[20px]" height={25} />
+        {links.map(({ href, label }) => (
+          <Link className={
+            pathname === href ? "font-medium" : undefined
+          } key={href} href={href}>
+            {label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
