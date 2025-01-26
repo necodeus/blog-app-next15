c9f9a6c8d1dd7e5437bf6de388bfbde7e4d6df19
Dawid Smulewicz <dawid@necodeo.com>
2025-01-26 01:13:17 +0100

    fix: set version

--- app/api/set-version/route.ts
+++ app/api/set-version/route.ts
@@ -1,21 +1,10 @@
 import { NextResponse } from "next/server";
 
-const versions = ["astro", "nuxt"];
-
-const getCookieValue = (cookies: string, key: string) => {
-  return cookies?.split("; ").find((cookie) => cookie.startsWith(`${key}=`))?.split("=")[1];
-};
-
-const getNewVersion = (currentVersion: string) => {
-  const currentIndex = versions.indexOf(currentVersion);
-  const nextIndex = (currentIndex + 1) % versions.length;
-  return versions[nextIndex];
-};
-
 export async function GET(req: Request) {
-  const nextVersion = getNewVersion(getCookieValue(req.headers.get("cookie") || '', "version") || '');
+  const nextVersion = "nuxt";
 
   const response = NextResponse.json({ version: nextVersion });
+
   response.cookies.set("version", nextVersion, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
